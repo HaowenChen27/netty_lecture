090b8c7d1e428d93d26cd1aac5169cbc453b56fa
chenhaowen <1661546232@qq.com>
2020-11-06 18:39:16 +0800

    第一次提交

---
+++ src/main/java/com/chenhaowen/demo/NettyServerTest.java
@@ -0,0 +1,23 @@
+package com.chenhaowen.demo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author chenhaowen
+ * @description
+ * @date 11/6/20  6:23 PM
+ */
+public class NettyServerTest {
+
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                .childHandler(null);
+    }
+}
