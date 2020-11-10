d88d9ca10eff5057630089a3eff9899574cb252f
chenhaowen <1661546232@qq.com>
2020-11-10 09:55:07 +0800

    提交代码

---
+++ src/main/java/com/chenhaowen/demo5/WebSocketServerInitializer.java
@@ -0,0 +1,27 @@
+package com.chenhaowen.demo5;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @author chenhaowen
+ * @description
+ * @date 11/9/20  3:08 PM
+ */
+public class WebSocketServerInitializer extends ChannelInitializer<NioSocketChannel> {
+
+    @Override
+    protected void initChannel(NioSocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new ChunkedWriteHandler());
+        pipeline.addLast(new HttpObjectAggregator(8192));
+        pipeline.addLast(new WebSocketServerProtocolHandler("/ws3"));
+        pipeline.addLast(new TextWebSocketFrameHandler());
+    }
+}
